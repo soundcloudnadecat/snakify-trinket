08a8833902c99da8c8839ce5ede6f178a3724129
Payton Smith <payton.smith@student.rjuhsd.us>
2018-10-23 20:58:23 +0000

    yeet

--- trinket.py
+++ trinket.py
@@ -3,3 +3,20 @@ a = int(input())
 b = int(input())
 c = int(input())
 print(a + b + c)
+
+# Hi John
+name = input()
+print("Hi", name)
+
+# Square
+number = int(input())
+print(number ** 2)
+
+# Area of right-angled triangle
+base = int(input())
+height = int(input())
+print((base * height) / 2)
+
+# Hello, Harry!
+name = input()
+print("Hello, " + name + "!")
