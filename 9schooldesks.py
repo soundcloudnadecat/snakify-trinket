ef95c3769b121b49a9cc8522258b68e71332176f
Payton Smith <payton.smith@student.rjuhsd.us>
2018-10-24 20:54:41 +0000

    Unit 1

--- 9schooldesks.py
+++ 9schooldesks.py
@@ -2,4 +2,4 @@
 class1 = int(input())
 class2 = int(input())
 class3 = int(input())
-print(a // 2 + b // 2 + c // 2 + a % 2 + b % 2 + c % 2)
+print(class1 // 2 + class2 // 2 + class3 // 2 + class1 % 2 + class2 % 2 + class3 % 2)
