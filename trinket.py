c31effa67cc32c56d14954f3c7d1542957406e43
Payton Smith <payton.smith@student.rjuhsd.us>
2018-10-24 20:29:12 +0000

    Unit 1

--- trinket.py
+++ trinket.py
@@ -20,3 +20,30 @@ print((base * height) / 2)
 # Hello, Harry!
 name = input()
 print("Hello, " + name + "!")
+
+# Apple sharing
+students = int(input())
+apples = int(input())
+print(apples // students)
+print(apples % students)
+
+# Previous and next
+number = int(input())
+print("The next number for the number", number, " is", number + 1)
+print("The previous number for the number", number, " is", number - 1)
+
+# Two timestamps
+a=int(input())
+b=int(input())
+c=int(input())
+x=int(input())
+y=int(input())
+z=int(input())
+g=((x-a)*3600+(y-b)*60+(z-c))
+print(g)
+
+# School desks
+class1 = int(input())
+class2 = int(input())
+class3 = int(input())
+print(a // 2 + b // 2 + c // 2 + a % 2 + b % 2 + c % 2)
